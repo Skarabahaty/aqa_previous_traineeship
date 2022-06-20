9ac1f2b02483c00168676f1a6e3f82cdf73a25d3
Maksim Skarabahaty <skaregok@gmail.com>
2022-06-20 13:14:15 +0300

    6 step done

---
+++ src/test/java/tests/test_cases/TestCase6.java
@@ -0,0 +1,43 @@
+package tests.test_cases;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import models.user.User;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+import utils.UnirestObjectsUtil;
+
+import java.util.LinkedList;
+
+public class TestCase6 extends BaseTest {
+
+    @Test
+    public void testGetUsers() throws UnirestException {
+
+        String mainPage = configData.getString("main_page");
+        String testCase = testData.getString("case_6");
+        String testPage = mainPage.concat(testCase);
+
+        HttpResponse<JsonNode> jsonNodeHttpResponse =
+                Unirest.get(testPage)
+                        .asJson();
+
+        int status = jsonNodeHttpResponse.getStatus();
+        int expectedStatus = testData.getInt("correct_get_code");
+        Assert.assertEquals(status, expectedStatus, "status isn't correct");
+
+        LinkedList<User> usersFromResponse = UnirestObjectsUtil.getUsersFromResponse(jsonNodeHttpResponse);
+
+        int neededId = testData.getInt("case_6_id");
+        User actualUser = UnirestObjectsUtil.getUserFromListById(usersFromResponse, neededId);
+
+        JSONObject expectedUserJSON = (JSONObject) testData.getObject("case_5_user");
+        User expectedUser = new User(expectedUserJSON);
+
+        Assert.assertEquals(actualUser, expectedUser, "users aren't equal");
+    }
+}
