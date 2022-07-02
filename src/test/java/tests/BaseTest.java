3285aeaedd4a28bf9973de8d52e56a3f2375e63c
Maksim Skarabahaty <skaregok@gmail.com>
2022-07-02 16:03:49 +0300

    jdk 11

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -25,7 +25,7 @@ public abstract class BaseTest {
     @BeforeMethod
     protected void beforeMethod() {
         browser = AqualityServices.getBrowser();
-        browser.setWindowSize(1920, 1080 );
+        browser.maximize();
     }
 
     @AfterMethod
