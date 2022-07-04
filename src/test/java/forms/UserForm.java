9eeb85cce19828cfe99671be0fb152248a47725b
Maksim Skarabahaty <skaregok@gmail.com>
2022-07-04 16:42:15 +0300

    fixes in locators

--- src/test/java/forms/UserForm.java
+++ src/test/java/forms/UserForm.java
@@ -9,20 +9,24 @@ import org.openqa.selenium.By;
 
 public class UserForm extends Form {
 
-    private final String likeButtonByPostIdDraft = "//div[contains(@id, \"%d\")]//div[contains(@class, \"PostBottomActionContainer \")]";
+    private final String likeButtonByPostIdDraft = "//div[contains(@id, '%d')]//div[contains(@class, 'PostBottomActionContainer ')]";
+    private final String wallPostLocatorDraft = "//div[contains(@class, 'wall_post_text') and contains(text(), '%s')]//ancestor::div[contains(@data-post-id, '%d')]";
+    private final String showNextCommentLinkXpathDraft = "//a[contains(@onclick, 'showNextReplies') and contains(@onclick, '%d')]//span[text()]";
+    private final By likeCounterLocator = By.xpath("//div[contains(@class, 'counter') and contains(text(), '')]");
+    private final String commentXpathExpressionDraft = "//div[contains(@id, '%d') and contains(@id, 'post')]";
+    private final String commentContentXpathExpressionDraft = "//div[contains(@id, '%d')]//div[@class='wall_reply_text']";
+    private final String commentAuthorXpathExpression = "//a[@class='author']";
 
     public UserForm() {
         super(By.id("wide_column"), "user page");
     }
 
     public ILabel getWallPostByTextAndPostId(String text, int postId) {
-        String wallPostLocatorDraft = "//div[contains(@class, \"wall_post_text\") and contains(text(), \"%s\")]//ancestor::div[contains(@data-post-id, \"%d\")]";
         String wallPostXpathExpression = String.format(wallPostLocatorDraft, text, postId);
         return getElementFactory().getLabel(By.xpath(wallPostXpathExpression), "wall post", ElementState.DISPLAYED);
     }
 
     public void clickShowNextCommentLink(int recordId) {
-        String showNextCommentLinkXpathDraft = "//a[contains(@onclick, \"showNextReplies\") and contains(@onclick, \"%d\")]//span[text()]";
         String xpathExpression = String.format(showNextCommentLinkXpathDraft, recordId);
         ILabel showNextCommentLink = getElementFactory().getLabel(By.xpath(xpathExpression), "show next comment link", ElementState.DISPLAYED);
         showNextCommentLink.click();
@@ -37,20 +41,19 @@ public class UserForm extends Form {
     public void waitForLikeToAppear(int postId) {
         String xpathExpression = String.format(likeButtonByPostIdDraft, postId);
         IButton likeButton = getElementFactory().getButton(By.xpath(xpathExpression), "like button", ElementState.DISPLAYED);
-        ILabel likeCounter = likeButton.findChildElement(By.xpath("//div[contains(@class, \"counter\") and contains(text(), \"\")]"), ILabel.class);
+        ILabel likeCounter = likeButton.findChildElement(likeCounterLocator, ILabel.class);
         likeCounter.state().waitForDisplayed();
     }
 
     public Comment findCommentByIdAndReturnIt(int commentId) {
-        String commentExpression = String.format("//div[contains(@id, \"%d\") and contains(@id, \"post\")]", commentId);
+        String commentExpression = String.format(commentXpathExpressionDraft, commentId);
         ILabel comment = getElementFactory().getLabel(By.xpath(commentExpression), "comment");
 
-        String commentContentExpression = String.format("//div[contains(@id, \"%d\")]//div[@class=\"wall_reply_text\"]", commentId);
+        String commentContentExpression = String.format(commentContentXpathExpressionDraft, commentId);
         ILabel commentContent = comment.findChildElement(By.xpath(commentContentExpression), ILabel.class);
         String commentText = commentContent.getText();
 
-        String commentAuthorExpression = "//a[@class=\"author\"]";
-        ILabel commentAuthor = comment.findChildElement(By.xpath(commentAuthorExpression), ILabel.class);
+        ILabel commentAuthor = comment.findChildElement(By.xpath(commentAuthorXpathExpression), ILabel.class);
         String dataFromId = commentAuthor.getAttribute("data-from-id");
         int commentAuthorId = Integer.parseInt(dataFromId);
 
